9b46050dda5857f4a76afc1b688cf7b5578fcd3c
1br0h1m-cyber <normahmatovibrohim0409@gmail.com>
2025-01-21 14:19:24 +0500

    reload

--- src/components/main/home.jsx
+++ src/components/main/home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
     <div id='home' className='flex justify-center items-center relative z-30'>
       <img src={homeImage} alt="home" title='3-maktab' className='w-[90%] md:w-[80%] h-[70vh] md:h-[90vh] filter contrast-150 brightness-75'/>
       <div className='absolute h-full w-[90%] md:w-[80%] bg-[rgba(0,0,0,0.25  )] flex flex-col justify-center items-center'>
-        <h1 className='text-[35px] md:text-[50px] capitalize text-white'>Find your <span className='bg-[#E5B300] px-1'>future</span> today!</h1>
-        <h3 className='text-[15px] md:text-[25px] capitalize text-white'>The ultimate guide to universities worldwide</h3>
+        <h1 className='text-[27px] md:text-[40px] lg:text-[50px] capitalize text-white pl-5 sm:pl-0'>Find your <span className='bg-[#E5B300] px-1'>future</span> today!</h1>
+        <h3 className='text-[15px] md:text-[20px] lg:text-[27px] capitalize text-white pl-5 sm:pl-0'>The ultimate guide to universities worldwide</h3>
       </div>
     </div>
   )
